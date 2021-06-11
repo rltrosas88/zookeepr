894628c67de6902a41a69d4b7b21b4b039727c1d
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-06-11 15:00:27 -0500

    3.6 create routes to serve other pages

--- server.js
+++ server.js
@@ -159,6 +159,22 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+//the * will act as a wildcard, 
+  //meaning any route that wasn't previously defined will fall under this request and will receive the hompage as the response.
+  //the order of routes matters!
+    //the * route should always come last or it will take precendence over named routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
